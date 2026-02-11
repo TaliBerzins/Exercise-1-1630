d3860227aa5cc5e0d8c0e0253ee76e5d9c672beb
TaliBerzins <tali.berzins@outlook.com>
2026-02-11 00:14:13 -0500

    Finalized cs file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,19 @@ Console.Write("Please enter the cost per square foot of flooring selected: ");
 int sqFootCost = Convert.ToInt32(Console.ReadLine());
 
 String dashes = new String( '-', 20);
-Console.Write(dashes);
+Console.WriteLine(dashes);
+
+const double feetPerHour = 40;
+const double costPerHour = 40;
+int floorSize = floorLength * floorWidth;
+double floorCost = sqFootCost * floorSize;
+double installationTime = floorSize / feetPerHour;
+double installationCost = costPerHour * installationTime;
+
+Console.WriteLine("With a total floor size of " + floorSize + " the flooring cost is $" + floorCost);
+Console.WriteLine("It will take " + installationTime + " hours to install the floor at a cost of $" + installationCost + "\n");
+Console.WriteLine("The total finished cost for the new floor is : $" + (installationCost + floorCost));
+
 
 
 
