3dd5f41ef0877579c5064bc9132b546bba5edd85
TaliBerzins <tali.berzins@outlook.com>
2026-02-10 23:40:09 -0500

    Initial code written

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Dr. I Flooring Cost Estimator\n: ");
+Console.Write("Please enter the length of floor: ");
+int floorLength = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the width of floor: ");
+int floorWidth = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the cost per square foot of flooring selected: ");
+int sqFootCost = Convert.ToInt32(Console.ReadLine());
+
+String dashes = new String( '-', 20);
+Console.Write(dashes);
+
+
+
